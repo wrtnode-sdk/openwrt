5bd3e812c9fd2682a731a2c2baff5a7a03ee89b6
xzmu <xzmu@wrtnode.com>
2015-10-24 13:42:45 +0800

    change default package

--- target/linux/ramips/mt7620/target.mk
+++ target/linux/ramips/mt7620/target.mk
@@ -3,13 +3,13 @@
 #
 
 SUBTARGET:=mt7620
-BOARDNAME:=MT7620 based boards
+BOARDNAME:=WRTnode 1 series
 ARCH_PACKAGES:=ramips_24kec
 FEATURES+=usb
 CPU_TYPE:=24kec
 CPU_SUBTYPE:=dsp
 
-DEFAULT_PACKAGES += kmod-rt2800-pci kmod-rt2800-soc
+DEFAULT_PACKAGES += ralink-wifi
 
 define Target/Description
 	Build firmware images for Ralink MT7620 based boards.
--- target/linux/ramips/mt7628/target.mk
+++ target/linux/ramips/mt7628/target.mk
@@ -3,11 +3,13 @@
 #
 
 SUBTARGET:=mt7628
-BOARDNAME:=MT7628 based boards
+BOARDNAME:=WRTnode 2 series
 ARCH_PACKAGES:=ramips_24kec
 FEATURES+=usb
 CPU_TYPE:=24kec
 CPU_SUBTYPE:=dsp
+
+DEFAULT_PACKAGES += ralink-wifi-mt76x8
 
 define Target/Description
 	Build firmware images for Ralink MT7628 based boards.
