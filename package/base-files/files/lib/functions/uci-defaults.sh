e7e1c83e7e826c5d4f4eed495f287fbce241fd9f
xzmu <xzmu@wrtnode.com>
2015-10-24 13:38:54 +0800

    change default ip to 192.168.8.1

--- package/base-files/files/lib/functions/uci-defaults.sh
+++ package/base-files/files/lib/functions/uci-defaults.sh
@@ -213,9 +213,8 @@ set network.lan.ifname='$ifname'
 set network.lan.force_link=1
 set network.lan.type='bridge'
 set network.lan.proto='static'
-set network.lan.ipaddr='192.168.1.1'
+set network.lan.ipaddr='192.168.8.1'
 set network.lan.netmask='255.255.255.0'
-set network.lan.ip6assign='60'
 EOF
 }
 
@@ -226,9 +225,6 @@ ucidef_set_interface_wan() {
 set network.wan='interface'
 set network.wan.ifname='$ifname'
 set network.wan.proto='dhcp'
-set network.wan6='interface'
-set network.wan6.ifname='$ifname'
-set network.wan6.proto='dhcpv6'
 EOF
 }
 
